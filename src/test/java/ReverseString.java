e9b3383542efdb8ce469182e80f267cbf93a8622
jzeynalov <71579394+jzeynalov@users.noreply.github.com>
2022-06-15 12:48:25 -0600

    GitHub practice due Monday 06/20/2022

--- src/test/java/ReverseString.java
+++ src/test/java/ReverseString.java
@@ -1,21 +1,23 @@
 public class ReverseString {
 
     public static void main(String[] args) {
+        
+        // conventional way through for loop  
         // https://www.geeksforgeeks.org/reverse-a-string-in-java/
 
-        String str = "Codytex", rev = "";
-        char ch;
+        String str = "Codytex", reversed = "";
+        char string;
 
         System.out.print("Original word: ");
         System.out.println("Codytex"); //Example word
 
         for (int i = 0; i < str.length(); i++) {
-            ch = str.charAt(i);
-            rev = ch + rev;
+            string = str.charAt(i);
+            reversed = string + reversed;
         }
-        System.out.println("Reversed word: " + rev);
+        System.out.println("Reversed word: " + reversed);
 
-        // easy way
+        // easy way but not acceptable during the interview
 
         String word = "Codytex";
         StringBuffer my1 = new StringBuffer(word);
